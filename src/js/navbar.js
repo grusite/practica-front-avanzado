9580620b6fb11be335ba41bfebbb56f01d58bbe8
Jorge Martín <grusite@gmail.com>
2019-09-17 00:59:07 +0200

    added searched rended and get filter in localstorage

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,12 +1,20 @@
 import { toggle } from './ui.js'
+import storage from './storage.js'
+import renderBeersDOM from './beers.js'
+
+const { setItem, getItem } = storage()
 
 const searchIcon = document.querySelector('#navbar-search')
 const inputSearch = document.querySelector('#search-form .input')
 const closeIcon = document.querySelector('#navbar-close')
+const searchForm = document.querySelector('#search-form')
+const searchInput = document.querySelector('.navbar .input.search')
 
 const handleSearchIcon = toggle(searchIcon)
 const handleInputSearch = toggle(inputSearch)
 const handleCloseIcon = toggle(closeIcon)
+
+searchInput.value = getItem('navbar-input')
 
 searchIcon.addEventListener('click', () => {
   handleSearchIcon('show', 'no-show')
@@ -25,5 +33,14 @@ const hideSearchFilter = () => {
   handleInputSearch('show', 'no-show')
   handleCloseIcon('show', 'no-show')
 }
+
+searchForm.addEventListener('submit', evt => {
+  evt.preventDefault()
+  if (searchInput.validity.valid) {
+    //  Render Shows
+    setItem('navbar-input', searchInput.value)
+    renderBeersDOM(searchInput.value)
+  }
+})
 
 export { hideSearchFilter }
---
+++ src/js/storage.js
@@ -0,0 +1,29 @@
+const lStorage = {
+  setItem: (key, value) => localStorage.setItem(key, value),
+  getItem: key => localStorage.getItem(key),
+}
+
+const sessionStorage = {
+  setItem: (key, value) => sessionStorage.setItem(key, value),
+  getItem: key => sessionStorage.getItem(key),
+}
+
+const cookieStorage = {
+  setItem: (key, value) => Cookies.set(key, value),
+  getItem: key => Cookies.get(key) || '',
+}
+
+const storage = (type = 'lStorage') => {
+  const types = {
+    lStorage,
+    cookieStorage,
+    sessionStorage,
+  }
+  if (typeof Storage !== 'undefined') {
+    // Se acepta localStorage
+    return types[type]
+  }
+  return types['cookieStorage']
+}
+
+export default storage
